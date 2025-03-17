fc1171b488468d1364040d1e9668aedec0e37213
William Blake Galbreath <Blake.Galbreath@GMail.com>
2025-03-17 12:28:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NoStamina
+
+A simple mod that disables stamina usage.
+
+There are no configs, no toggles, no messages, or flashy lights. It just works.
+
+No, I will not put a toggle. If you want stamina to work, just remove the mod...
---
+++ src/NoStaminaMod.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace NoStamina {
+    [BepInPlugin("billy.NoStamina", "NoStamina", "0.0.1")]
+    public class NoStaminaMod : BaseUnityPlugin {
+        protected void Awake() {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch]
+    internal class StaminaPatch {
+        [HarmonyPatch(typeof(Player), "UseStamina"), HarmonyPostfix]
+        private static void PostUseStamina(ref float ___m_stamina) {
+            ___m_stamina = 1000f;
+        }
+    }
+}
